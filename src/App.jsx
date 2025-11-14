7bec5c5236316535e59f289a0c3d978496caefd4
Josoa Vonjiniaina <76913187+josoavj@users.noreply.github.com>
2025-11-14 21:22:12 +0300

    update: Ajout des pages Paramètres et Gestion utilisateur

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { ThemeProvider, CssBaseline } from '@mui/material'
 import { SignUpComponent } from './components/SignUpComponent'
 import { LogInComponent } from './components/LogInComponent'
 import { DataVisualization } from './components/DataVisualization'
+import UserManagement from './components/UserManagement'
+import SettingsPage from './components/SettingsPage'
 import theme from './theme'
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
                     <Route path="/auth/signup" element={<SignUpComponent />} />
                     <Route path="/auth/login" element={<LogInComponent />} />
                     <Route path="/visualization" element={<DataVisualization />} />
+                    <Route path="/admin/users" element={<UserManagement />} />
+                    <Route path="/settings" element={<SettingsPage />} />
                 </Routes>
             </BrowserRouter>
         </ThemeProvider>
