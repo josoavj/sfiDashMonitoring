988b15d1f884f992e1bb40aef782deabd9bd317b
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-02-08 22:53:11 +0300

    Phase 1: Ajouter configuration Vitest pour tests frontend automatisés

---
+++ vitest.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'json'],
+      exclude: [
+        'node_modules/',
+        'src/test/'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
